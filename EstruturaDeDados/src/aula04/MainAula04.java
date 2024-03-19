e8b2b79dc1e8e5138f4583a5840de253d019f2b2
Otavio Marques <marquesotavio251204@gmail.com>
2024-03-18 22:19:46 -0300

    classe pessoa

---
+++ EstruturaDeDados/src/aula04/MainAula04.java
@@ -0,0 +1,8 @@
+package aula04;
+
+public class MainAula04 {
+    public static void main(String[] args) {
+        Pessoa pessoa = new Pessoa("Otavio", 19);
+        pessoa.imprimirDetalhes();
+    }
+}
