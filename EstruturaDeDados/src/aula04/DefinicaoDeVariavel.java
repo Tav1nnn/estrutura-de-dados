79b97434e4261bffdd810f8e0428bdb2e193cb24
Otavio Marques <marquesotavio251204@gmail.com>
2024-03-18 22:15:35 -0300

    definição de variavel

---
+++ EstruturaDeDados/src/aula04/DefinicaoDeVariavel.java
@@ -0,0 +1,16 @@
+package aula04;
+
+public class DefinicaoDeVariavel {
+    public static void main(String[] args) {
+        int numInt = 99;
+        double numDouble = 99.99;
+        boolean bool = true;
+        String text = "otavio";
+
+        System.out.println("Int "+numInt
+                + "\nDouble "+numDouble
+                + "\nBoolean "+bool
+                + "\nString "+text
+        );
+    }
+}
